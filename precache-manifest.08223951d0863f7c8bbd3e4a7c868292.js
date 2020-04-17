15855f6b10ff3183c8e751a613a89e19bcdc68d1
PetrRNNT <blackrnnt@gmail.com>
2020-04-17 16:25:36 +0200

    Updates

---
+++ precache-manifest.08223951d0863f7c8bbd3e4a7c868292.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdf1367a8416a061db6f3e5cc19908e",
+    "url": "/PetrRNNT/demo-social-react/index.html"
+  },
+  {
+    "revision": "813f3c1d42faf753a3e2",
+    "url": "/PetrRNNT/demo-social-react/static/css/main.98450a99.chunk.css"
+  },
+  {
+    "revision": "3448d8d5988b30fd3ada",
+    "url": "/PetrRNNT/demo-social-react/static/js/2.6c7ff3b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PetrRNNT/demo-social-react/static/js/2.6c7ff3b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "813f3c1d42faf753a3e2",
+    "url": "/PetrRNNT/demo-social-react/static/js/main.ab27c0ee.chunk.js"
+  },
+  {
+    "revision": "14ce525706a5d7874f22",
+    "url": "/PetrRNNT/demo-social-react/static/js/runtime-main.2c026d60.js"
+  },
+  {
+    "revision": "7c98d8d8d3808e27eee99ffc89628063",
+    "url": "/PetrRNNT/demo-social-react/static/media/background.7c98d8d8.png"
+  },
+  {
+    "revision": "0fc9cd92d02c23947dcffeec4cd0dc18",
+    "url": "/PetrRNNT/demo-social-react/static/media/preloader.0fc9cd92.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/PetrRNNT/demo-social-react/precache-manifest.08223951d0863f7c8bbd3e4a7c868292.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PetrRNNT/demo-social-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
